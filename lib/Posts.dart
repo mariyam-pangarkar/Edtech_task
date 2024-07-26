3a8ec3cbcffef32d4094e4c9fb5c54169a272307
mariyam-pangarkar <mariyampangarkar259@email.com>
2024-07-26 07:58:12 +0530

    task done

--- lib/Posts.dart
+++ lib/Posts.dart
@@ -1,4 +1,3 @@
-import 'package:edtech_task/signupevent.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
--- lib/signupevent.dart
+++ lib/signupevent.dart
@@ -1,5 +1,4 @@
 import 'package:edtech_task/imageclip.dart';
-import 'package:edtech_task/suggestion.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
